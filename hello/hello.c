dda9bea35cef6a7f0b6b1e19a765f86b1b29098a
elisa768 <elyscant@gmail.com>
2024-12-20 15:44:09 +0000

    created hello

---
+++ hello/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //std input output
+
+int main() {
+    printf("hello world!\n"); // print format   // \n new line
+    return 0; //valore di ritorno per compilazione corretta 
+}
