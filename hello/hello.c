21e4d3122d2de74064b661c370de4a46fca1fa63
elisa768 <elyscant@gmail.com>
2024-12-20 16:08:46 +0000

    hello.c updated

--- hello/hello.c
+++ hello/hello.c
@@ -2,5 +2,12 @@
 
 int main() {
     printf("hello world!\n"); // print format   // \n new line
+
+    char name[32]; //variabile che può contenere fino a 32 caratteri
+
+    printf("Enter a name: ");
+    scanf("%s",name);
+    printf("Hello\t%s", name); // \t è il carattere di escape per tabulazione 
+
     return 0; //valore di ritorno per compilazione corretta 
 }
