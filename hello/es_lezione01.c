1edde60dc8fbbb91c0af507e60ffa0f4bf697da8
elisa768 <elyscant@gmail.com>
2025-01-14 14:46:23 +0100

    Update es_lezione01.c

--- hello/es_lezione01.c
+++ hello/es_lezione01.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-
-int main(){
-
-    char name[32];
-    char surname[32];
-
-    printf("Enter name and surname: ");
-    scanf("%s", name);
-    scanf("%s", surname);
-    printf("Hello%s", name+surname);
-    printf("Merry Christmas and a happy new year");
-
-    return 0;
-
+int main()
+{
+	char nome[40], cognome[40];
+	printf("inserisci il tuo nome: ");
+	scanf("%s", nome);
+	printf("\ninserisci il tuo cognome: ");
+	scanf("%s", cognome);
+	printf("\nbenvenuto, %s %s!\nGrazie per aver usato il nostro programma.\nTi auguriamo Buone Feste", nome, cognome);
+	printf("\n\n\t   \n\t  *\n\t **\n\t***\n\t  *");
+	
+	return 0;
 }
