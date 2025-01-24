be8b8d96ff892b8d2a4f662a7ef32f1bde34cc0c
elisa768 <elyscant@gmail.com>
2025-01-24 15:35:30 +0000

    modifica2

---
+++ hello/compito03.h
@@ -0,0 +1,12 @@
+#ifndef _DEFINE_H_
+#define _DEFINE_H_
+
+#define Nome "Elisa Scantamburlo"
+#define stampa_nome printf("%s", Nome)
+#define somma(x,y) x+y
+#define sottr(x,y) x-y
+#define molt(x,y) x*y
+#define div(x,y) x/y
+#define mod(x,y) x%y
+
+#endif _DEFINE_H_
